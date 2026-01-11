938e9b5064a4055400ef5c9c0083678f5236c0e5
Arun Kumar <ak3930566@gmail.com>
2026-01-11 13:25:10 +0530

    Setup routing and global styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,31 @@
-function App() {
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
-  return  (
-    <>
-      Hello world
-    </>
-  )
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
