ea25e801dd0daf260b1f92d1f50566a4629d8a4c
Arun <ak3930566@gmail.com>
2026-01-15 23:22:57 +0530

    fix: correct Nav import path and adjust Layout component styling

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,9 +1,9 @@
 import { Outlet } from 'react-router';
-import Nav from './nav';
+import Nav from './nav/Index';
 
 export default function Layout() {
     return (
-        <div className="min-h-screen flex flex-col">
+        <div className="min-h-screen flex flex-col max-w-4xl mx-auto">
             <Nav />
             <main className="flex-1">
                 <Outlet />
